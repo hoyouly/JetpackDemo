7492b6cdd47d96ccd5ce10762d97445a0f91ff6b
helei <heleik@digitalchina.com>
2020-09-10 17:22:39 +0800

    首页添加FloatingButton,可以根据品牌过滤

---
+++ app/src/main/java/com/hoyouly/jetpackdemo/common/CommonKtx.kt
@@ -0,0 +1,8 @@
+package com.hoyouly.jetpackdemo.common
+
+/**
+ * @ Time  :  2020-09-10
+ * @ Author :  helei
+ * @ Email :   heleik@digitalchina.com
+ * @ Description :
+ */
---
+++ app/src/main/java/com/hoyouly/jetpackdemo/common/listener/SimpleAnimatorListener.kt
@@ -0,0 +1,25 @@
+package com.hoyouly.jetpackdemo.common.listener
+
+import android.view.animation.Animation
+
+/**
+ * @ Time  :  2020-09-10
+ * @ Author :  helei
+ * @ Email :   heleik@digitalchina.com
+ * @ Description :
+ */
+
+class SimpleAnimation :Animation.AnimationListener{
+    override fun onAnimationRepeat(animation: Animation?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onAnimationEnd(animation: Animation?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onAnimationStart(animation: Animation?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
